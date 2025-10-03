60ffdf3226762b1e7bee54aa0a4a5717cb4dc0c6
Vandu01 <vandanajnvalwar@gmail.com>
2025-10-04 00:28:54 +0530

    frontend

---
+++ frontend-vite/src/main.jsx
@@ -0,0 +1,32 @@
+import { createContext, StrictMode, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Define Context
+export const Context = createContext({ isAuthorized: false });
+
+// eslint-disable-next-line react-refresh/only-export-components
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState({});
+
+  return (
+    <Context.Provider
+      value={{
+        isAuthorized,
+        setIsAuthorized,
+        user,
+        setUser,
+      }}
+    >
+      <App />
+    </Context.Provider>
+  );
+};
+
+// Render
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppWrapper />
+  </StrictMode>
+);
